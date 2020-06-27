b873840cc457647d2b03bdeb1201c05ea89ebe1b
felipefinity <felipe-augusto1997@hotmail.com>
2020-06-27 01:29:29 -0300

    Classes Position, Piece e Board created

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	public Board getBoard() {
+		return board;
+	}	
+}
